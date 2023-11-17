2f3bd6e843f615e4342647e01390a82b98360068
G0blinC0de <brewbrotherred@gmail.com>
2023-11-17 10:12:36 -0800

    add numScan function to break input into array

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,13 @@
 // Business Logic
 
 
+function numScan() {
+    const numInput = document.getElementById('numInput');
+    const inputArray = numInput.onsecuritypolicyviolation(" ");
+    let numResult = numDetect(inputArray);
+    displayText(numResult);
+
+}
 
 
 // UI Logic
